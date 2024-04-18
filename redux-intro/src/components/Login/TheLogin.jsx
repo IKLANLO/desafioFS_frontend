da2bf445f7e6a580fd8f3ba7bc6fd85631ef03d5
danirus6 <danirus6@gmail.com>
2024-04-18 15:30:08 +0200

    loginDiseño v0.1

--- redux-intro/src/components/Login/TheLogin.jsx
+++ redux-intro/src/components/Login/TheLogin.jsx
@@ -1,61 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-  login as loginAlumno,
-  reset as resetAlumnos,
-} from '../../redux/alumnos/alumnosSlice'
-import { useNavigate } from 'react-router-dom'
-import ToggleSwitch from '../ToggleSwitch/ToggleSwitch.jsx'
-import {
-  login as loginOrganismo,
-  reset as resetOrganismos,
-} from '../../redux/organismos/organismosSlice.js'
-import { notification } from 'antd'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login as loginAlumno, reset as resetAlumnos } from '../../redux/alumnos/alumnosSlice';
+import { useNavigate } from 'react-router-dom';
+import ToggleSwitch from '../ToggleSwitch/ToggleSwitch.jsx';
+import { login as loginOrganismo, reset as resetOrganismos } from '../../redux/organismos/organismosSlice.js';
+import { notification } from 'antd';
+import "./TheLogin.css";
 
 const TheLogin = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const {
     isSuccess: isSuccessAlumno,
     isError: isErrorAlumno,
     message: messageAlumno,
     isLoading: isLoadingAlumno,
-  } = useSelector((state) => state.alum)
+  } = useSelector((state) => state.alum);
   const {
     isSuccess: isSuccessOrganismo,
     isError: isErrorOrganismo,
     message: messageOrganismo,
     isLoading: isLoadingOrganismo,
-  } = useSelector((state) => state.organ)
-  const [userType, setUserType] = useState('alumno')
+  } = useSelector((state) => state.organ);
+  const [userType, setUserType] = useState('alumno');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleUserTypeChange = (checked) => {
-    setUserType(checked ? 'organizacion' : 'alumno')
-    console.log(userType)
-  }
-  useEffect(() => {
-    dispatch(resetAlumnos(), dispatch(resetOrganismos()))
-  }, [])
+    setUserType(checked ? 'organizacion' : 'alumno');
+    console.log(userType);
+  };
 
   useEffect(() => {
-    // console.log(isSuccessAlumno, isSuccessOrganismo)
+    dispatch(resetAlumnos(), dispatch(resetOrganismos()));
+  }, []);
+
+  useEffect(() => {
     if (isSuccessAlumno) {
-      // console.log(isSuccessAlumno)
-
-      notification.success({ description: messageAlumno })
-      setTimeout(() => navigate('/Inicio'), 1000)
+      notification.success({ description: messageAlumno });
+      setTimeout(() => navigate('/Inicio'), 1000);
     } else if (isErrorAlumno) {
-      notification.error({ description: messageAlumno })
-      // dispatch(resetAlumnos())
+      setErrorMessage(messageAlumno);
     }
 
     if (isSuccessOrganismo) {
-      // console.log(isSuccessOrganismo)
-      notification.success({ description: messageOrganismo })
-      setTimeout(() => navigate('/InicioOrg'), 1000) // Ruta de organismo
+      notification.success({ description: messageOrganismo });
+      setTimeout(() => navigate('/InicioOrg'), 1000); // Ruta de organismo
     } else if (isErrorOrganismo) {
-      notification.error({ description: messageOrganismo })
-      // dispatch(resetOrganismos())
+      setErrorMessage(messageOrganismo);
     }
   }, [
     isSuccessAlumno,
@@ -64,53 +55,59 @@ const TheLogin = () => {
     isSuccessOrganismo,
     isErrorOrganismo,
     messageOrganismo,
-  ])
+  ]);
 
   const [formData, setFormData] = useState({
     Email: '',
     Password: '',
-  })
+  });
 
-  const { Email, Password } = formData
+  const { Email, Password } = formData;
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }))
-  }
+    }));
+  };
 
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (userType === 'alumno') {
-      console.log('alumno')
-      // console.log(formData)
-      dispatch(loginAlumno(formData))
+      dispatch(loginAlumno(formData));
     } else if (userType === 'organizacion') {
-      console.log('empresa')
-      // console.log(formData)
-      // console.log(isSuccessOrganismo)
-      dispatch(loginOrganismo(formData))
+      dispatch(loginOrganismo(formData));
     }
-  }
+  };
 
   return (
     <form onSubmit={onSubmit}>
-      <input type="email" name="Email" value={Email} onChange={onChange} />
+      <h2>Iniciar Sesión</h2>
+
+      <input
+        type="email"
+        name="Email"
+        value={Email}
+        onChange={onChange}
+        placeholder="Correo electrónico"
+      />
       <input
         type="password"
         name="Password"
         value={Password}
         onChange={onChange}
+        placeholder="Contraseña"
       />
 
       <ToggleSwitch
-        label="Organ"
+        label="Eres empresa?"
         onChange={handleUserTypeChange}
         checked={userType === 'organizacion'}
       />
       <button type="submit">Login</button>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
     </form>
-  )
-}
-export default TheLogin
+  );
+};
+
+export default TheLogin;
