cffc61c1b627ad495a0d4dd55319411873b49d0f
danirus6 <danirus6@gmail.com>
2024-04-18 18:35:43 +0200

    Profile 0.1

--- redux-intro/src/components/Profile/Profile.jsx
+++ redux-intro/src/components/Profile/Profile.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Modal, Form, Input, message, Row, Col } from 'antd';
 import { updateUser } from '../../redux/alumnos/alumnosSlice';
+import './Profile.css'; // Importamos el archivo de estilos
+import AvatarPerfil from '../../assets/images/unnamed.jpg'
 
 const Profile = () => {
     const dispatch = useDispatch();
@@ -44,6 +46,7 @@ const Profile = () => {
           CP: values.CP,
           Telefono: values.Telefono,
           Experiencia: values.Experiencia,
+          Sector: values.Sector
         };
       console.log(updatedData);
         dispatch(updateUser({ userId: user._id, userData: updatedData }));
@@ -55,77 +58,87 @@ const Profile = () => {
     };
 
     return (
-        <>
-            <Row gutter={[16, 16]}>
-                <Col span={8}>
-                    <h1>Mi Perfil</h1>
-                    <p>Nombre: {user && user.Nombre}</p>
-                    <p>Edad: {user && user.Edad}</p>
-                    <p>Sector: {user && user.Sector}</p>
-                </Col>
-                <Col span={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <img src="tu-imagen.jpg" alt="Mi Imagen" style={{ maxWidth: '100%', maxHeight: '100%' }} />
-                </Col>
-                <Col span={8}>
-                    <p>Email: {user && user.Email}</p>
-                    <p>Teléfono: {user && user.Telefono}</p>
-                    <p>Experiencia: {user && user.Experiencia}</p>
-                </Col>
-            </Row>
+        <div className="profile-container">
+        <Row gutter={[16, 16]}>
+            <Col xs={24} sm={24} md={24}>
+                <h1>Mi Perfil</h1>
+            </Col>
+        </Row>
 
-            <Row gutter={[16, 16]}>
-                <Col span={8}>
-                    <h2>Otros Detalles</h2>
-                    <p>Campo1: {user && user.Campo1}</p>
-                    <p>Campo2: {user && user.Campo2}</p>
-                </Col>
-                <Col span={8}>
-                    <p>Campo3: {user && user.Campo3}</p>
-                    <p>Campo4: {user && user.Campo4}</p>
-                </Col>
-                <Col span={8}>
-                    <p>Campo5: {user && user.Campo5}</p>
-                    <p>Campo6: {user && user.Campo6}</p>
-                </Col>
-            </Row>
+        <Row gutter={[16, 16]}>
+            <Col xs={24} sm={24} md={9}>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Nombre}</Button>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Edad}</Button>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Sector}</Button>
+            </Col>
+            <Col xs={24} sm={24} md={6}>
+                <div className="avatar-container">
+                    <img src={AvatarPerfil} alt="Mi Imagen" className="round-image" />
+                </div>
+            </Col>
+            <Col xs={24} sm={24} md={9}>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Email}</Button>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Telefono}</Button>
+                <Button className="orange-button first-row-button" onClick={() => setIsEditing(true)}>{user && user.Experiencia}</Button>
+            </Col>
+        </Row>
 
-            <Button onClick={() => setIsEditing(true)}>Editar Perfil</Button>
+        <Row gutter={[16, 16]} className="margin-top">
+            <Col xs={24} sm={24} md={24}>
+                <div className="avatar-container">
+                    <img src={AvatarPerfil} alt="Otra Imagen" className="image" />
+                </div>
+            </Col>
+        </Row>
 
-            <Modal
-                title="Editar Perfil"
-                visible={isEditing}
-                onCancel={handleCancel}
-                footer={[
-                    <Button key="cancel" onClick={handleCancel}>
-                        Cancelar
-                    </Button>,
-                    <Button form="editForm" key="submit" htmlType="submit">
-                        Guardar
-                    </Button>,
-                ]}
+        <Row gutter={[16, 16]} className="margin-top">
+            <Col xs={24} sm={24} md={24}>
+                <h2>Título de la Tercera Fila</h2>
+                <div className="avatar-container">
+                    <img src={AvatarPerfil} alt="Otra Imagen" className="image" />
+                </div>
+                <Button className="small-button orange-button">Botón</Button>
+            </Col>
+        </Row>
+
+        <Modal
+            title="Editar Perfil"
+            visible={isEditing}
+            onCancel={handleCancel}
+            footer={[
+                <Button key="cancel" onClick={handleCancel}>
+                    Cancelar
+                </Button>,
+                <Button form="editForm" key="submit" htmlType="submit">
+                    Guardar
+                </Button>,
+            ]}
             >
-                <Form id="editForm" onFinish={onFinish} initialValues={editedUser} onValuesChange={onValuesChange}>
-                    <Form.Item label="Nombre" name="Nombre">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Genero" name="Genero">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Email" name="Email">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="CP" name="CP">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Teléfono" name="Telefono">
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Experiencia" name="Experiencia">
-                        <Input />
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
+            <Form id="editForm" onFinish={onFinish} initialValues={editedUser} onValuesChange={onValuesChange}>
+                <Form.Item label="Nombre" name="Nombre">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Genero" name="Genero">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Email" name="Email">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="CP" name="CP">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Teléfono" name="Telefono">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Experiencia" name="Experiencia">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Sector" name="Sector">
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Modal>
+    </div> 
     );
 };
 
