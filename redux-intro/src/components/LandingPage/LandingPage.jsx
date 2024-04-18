329f7481d8347a8a8f853b0d3003f9068a8cc1c2
danirus6 <danirus6@gmail.com>
2024-04-18 11:02:10 +0200

    comentarios para que no pete

--- redux-intro/src/components/LandingPage/LandingPage.jsx
+++ redux-intro/src/components/LandingPage/LandingPage.jsx
@@ -15,9 +15,9 @@ const LandingPage = () => {
 
   return (
     <>
-    <h2>La fórmula perfecta para unir <span> talentos + empresas</span> en Bizkaia</h2>
+    <h2>La fórmula perfecta para unir <span> talentos +</span><span> empresas</span> en Bizkaia</h2>
     <p>4 científicos tech para crear la fórmula perfecta: unir talentos y empresas de la mano de BBK Fundazioa. Regístrate y comienza  a sumar tu talento, multiplicar tu experiencia e igualar tus oportunidades:</p>
-    <div className="tres">
+    {/* <div className="tres">
       <img>Sumatu talento</img>
       <img>Multiplica tu experiencia</img>
       <img>Iguala tus oportunidades</img>
@@ -25,7 +25,7 @@ const LandingPage = () => {
     <div className=" button-container">
     <button type="button" className="registrarme"></button>
     <button type="button" className="mas-info"></button>
-    </div>
+    </div> */}
     <div className="image-slider-container">
     <Slider {...settings}>
     <div>
